20e7ac667887e6dec78659a9c659236f08416414
Dimitry Ivanov <2@ivanoff.org.ua>
2023-09-09 22:46:20 +0300

    add types

--- typings.d.ts
+++ typings.d.ts
@@ -34,10 +34,35 @@ declare module 'react-native-ico-software-development' {
       'css' |
       'nodejs';
 
+    type backgroundType = 'circle' | 'rect' | 'button';
+
+    type positionType = 'top' | 'bottom' | 'right' | 'left' |
+      'top_right' | 'top_left' | 'bottom_right' | 'bottom_left';
+
+    type backgroundTypeObj = {
+      type?: backgroundType;
+      color?: string;
+      radius?: number;
+      borderRadius?: number;
+    };
+
+    type badgeTypeObj = {
+      value?: number | string;
+      fontSize?: string;
+      position?: positionType;
+      color?: string;
+      backgroundColor?: string;
+      radius?: number;
+    };
+
     interface IconProps {
       name: iconNames;
-      color: string;
-      size: number;
+      width?: number;
+      height?: number;
+      color?: string;
+      stroke?: number;
+      background?: backgroundType | backgroundTypeObj;
+      badge?: number | string | badgeTypeObj;
     }
 
     const Icon: (props: IconProps) => ReactNode;
@@ -45,4 +70,3 @@ declare module 'react-native-ico-software-development' {
     export { iconNames };
     export default Icon;
 }
-
