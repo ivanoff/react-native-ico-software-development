6ddd829313aed27d08a89745623049140267d48d
Dimitry Ivanov <2@ivanoff.org.ua>
2023-10-01 18:37:42 +0300

    fix types

--- typings.d.ts
+++ typings.d.ts
@@ -1,5 +1,6 @@
 declare module 'react-native-ico-software-development' {
     import { ReactNode } from 'react';
+    import { SvgProps, AdditionalProps } from 'react-native-svg';
 
     type iconNames = 'nginx' |
       'raspberry-pi' |
@@ -65,7 +66,7 @@ declare module 'react-native-ico-software-development' {
       badge?: number | string | badgeTypeObj;
     }
 
-    const Icon: (props: IconProps) => ReactNode;
+    const Icon: (props: IconProps & SvgProps & AdditionalProps) => ReactNode;
 
     export { iconNames };
     export default Icon;
